887120cc0984083b4870ccb6998cf971ae8ea59c
Vinod Singh Ramalwan <vinod.ramalwan@smart-data-deutschland.de>
2018-11-11 14:26:13 +0100

    DONE: Task 1 & 2

--- client/index.js
+++ client/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import { render } from 'react-dom';
-import Router from 'react-router'; 
+import {
+  render
+} from 'react-dom';
+import Router from 'react-router-dom';
 import Apps from './Apps';
-import './styles.css'; 
+import './styles.css';
 
 
 
 function rerender() {
   var route = window.location.hash.substr(1);
-  render(<div> 
-         <Apps header="Thing List for Apps"/>
-       </div> 
-       
-       , document.getElementById('application'));
+  render( < div >
+    <
+    Apps header = "Thing List for Apps" / >
+    <
+    /div> 
+
+    , document.getElementById('application'));
 }
 
 rerender();
